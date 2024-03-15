8181f8ce91827f466481b4582d0a6bb566a1343d
DESKTOP-E0PA0K3\Tim <lamp5241@gmail.com>
2024-03-15 18:09:20 +0800

    admin更新

--- src/main/resources/static/admin/js/basic.js
+++ src/main/resources/static/admin/js/basic.js
@@ -1,3 +1,52 @@
 
 
+//static
+const kCurrentLoginEmployeKey = 'currentLoginEmploye';
+const kLoginPath = '/admin/singIn/index.html';
+const kIndexPath = '/admin/index.html';
 
+/*
+Methods
+*/
+//檢查是否登入若沒有登入就導到登入頁
+function checkEmployeDidLogingAndReplaceURL() {
+    if (getCurrentLoginEmploye()==false) {
+        window.location.replace(kLoginPath);
+    }
+}
+
+function getCurrentLoginEmploye(){
+    var currentLoginEmploye;
+    var getCurrentLoginEmployeJsonString = window.sessionStorage.getItem(kCurrentLoginEmployeKey);
+    if (getCurrentLoginEmploye) {
+        currentLoginEmploye = JSON.parse(getCurrentLoginEmployeJsonString);
+    }
+    return currentLoginEmploye;
+}
+
+
+//存資料（導頁）
+function saveCurrentLoginEmploye(jsonString) {
+    window.sessionStorage.setItem(kCurrentLoginEmployeKey,jsonString)
+    window.location.replace(kIndexPath);
+}
+
+//登出 (清資料,導頁)
+function logoutCurrentLoginEmploye(){
+    window.sessionStorage.removeItem(kCurrentLoginEmployeKey);
+    window.location.replace(kLoginPath);
+}
+
+
+function showMessage(message) {
+    alert(message);
+}
+
+
+
+/*
+LifeCycel
+*/
+$(document).ready(function () {
+  checkEmployeDidLogingAndReplaceURL()
+});
